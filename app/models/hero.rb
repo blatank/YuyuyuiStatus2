8551fffe6b03873a090820d9229fdc19d83254ec
blatank <paddocknote@gmail.com>
2020-05-05 03:08:26 +0000

    Add hero model validation

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,12 @@
 class Hero < ApplicationRecord
+  # name validation
   validates :name, presence: true,
                      length: { maximum: 10 }
+  
+  # hero_type validation
+  VALID_HERO_TYPE_REGEX = /\A(巫女|遠射|範囲|近接)\z/
+  validates :hero_type, presence: true,
+                          length: { maximum: 2 },
+                          format: { with: VALID_HERO_TYPE_REGEX }
+  
 end
