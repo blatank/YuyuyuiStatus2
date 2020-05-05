4a62f565dc484169413306508195b3d5542c01c2
blatank <paddocknote@gmail.com>
2020-05-05 05:35:26 +0000

    Add csv import

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Hero < ApplicationRecord
   # name validation
   validates :name, presence: true,
@@ -9,4 +11,19 @@ class Hero < ApplicationRecord
                           length: { maximum: 2 },
                           format: { with: VALID_HERO_TYPE_REGEX }
   
+  # CSVからのインポート
+  def self.csv_import(dir_path = 'db/csv/*.csv')
+    Dir.glob(dir_path) do |f|
+      datas = CSV.open(f)
+      
+      # 先頭行だけデータから抜く
+      topRow   = datas.shift
+    
+      hero_name = topRow[0]
+      # [1]はファイル名に相当する箇所なので
+      hero_type = topRow[2]
+      
+      Hero.create!(name: hero_name, hero_type: hero_type)
+    end
+  end
 end
