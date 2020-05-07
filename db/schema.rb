1aa9cddc787499548358b7bcd77d754989fb1771
blatank <paddocknote@gmail.com>
2020-05-07 04:05:08 +0000

    hero_typeのmigrationファイルを強引にリネームしてmigrationの順番を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_005400) do
+ActiveRecord::Schema.define(version: 2020_05_05_055030) do
 
   create_table "hero_types", force: :cascade do |t|
     t.string "name"
