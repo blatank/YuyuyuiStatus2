d7d731b023366e967ba50582c6baf23f3f222883
blatank <paddocknote@gmail.com>
2020-05-11 14:03:39 +0000

    仮トップページその2

--- test/integration/home_page_access_test.rb
+++ test/integration/home_page_access_test.rb
@@ -15,15 +15,27 @@ class HomePageAccessTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", ssrs_path, count: 1
     
     ## 各勇者別のページへのリンクがある
-    heros = Hero.all
-    heros.each do |hero|
-      assert_select "a[href=?]", hero_path(hero), count: 1
-    end
+    assert_select "a[href=?]", hero_path(heros(:wassy)), count: 1
+    assert_select "a[href=?]", hero_path(heros(:gin)), count: 1
+
+    ## SSRがない勇者へのリンクはない
+    assert_select "a[href=?]", hero_path(heros(:sonochu)), count: 0
     
     ## 属性別へのリンクがある
     colors = Color.all
     colors.each do |color|
       assert_select "a[href=?]", color_path(color), text: color.mean, count: 1
     end
+    
+    ## 存在するタイプの勇者へのリンクがある
+    types = [:kinsetsu, :ensya, :hani]
+    types.each do |t|
+      hero_type = hero_types(t)
+      assert_select "a[href=?]", hero_type_path(hero_type), text: hero_type.name, count: 1
+    end
+    
+    ## 存在しないタイプの勇者へのリンクがない
+    assert_select "a[href=?]", hero_type_path(hero_types(:miko)), count: 0
+    
   end
 end
