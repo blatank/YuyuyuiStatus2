9fdfae0ca823b674572705d43d851c61008f1e19
blatank <paddocknote@gmail.com>
2020-05-31 03:00:10 +0000

    filenameの初期値を修正

--- db/migrate/20200531012259_add_filename_to_heros.rb
+++ db/migrate/20200531012259_add_filename_to_heros.rb
@@ -1,5 +1,5 @@
 class AddFilenameToHeros < ActiveRecord::Migration[6.0]
   def change
-    add_column :heros, :filename, :string, default: "hero.csv"
+    add_column :heros, :filename, :string, default: "hero"
   end
 end
